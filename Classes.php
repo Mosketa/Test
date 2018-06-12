1522cdaaa2d23176ad2eaac615e6ee1c016fc48c
Mosketa <moschetta.adrien@live.fr>
2018-06-12 10:21:19 +0200

    New class Vegetable
    Add new class vegetable

---
+++ Classes.php
@@ -0,0 +1,26 @@
+<?php
+
+// classe de base, avec des propriétés et des méthodes membres
+class Vegetable {
+
+   var $edible;
+   var $color;
+
+   function __construct($edible, $color="green")
+   {
+       $this->edible = $edible;
+       $this->color = $color;
+   }
+
+   function is_edible()
+   {
+       return $this->edible;
+   }
+
+   function what_color()
+   {
+       return $this->color;
+   }
+} // fin de la classe Vegetable
+
+?>
